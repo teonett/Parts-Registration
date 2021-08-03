c84dc0c7cdd6ad4a46be81a536533bba0abaad20
Jose Teotonio <teonett@gmail.com>
2021-08-03 18:10:38 -0300

    Implement structure

---
+++ src/Parts.Registration.Core/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parts.Registration.Core
+{
+    public abstract class BaseEntity<TId>
+    {
+        public TId Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ src/Parts.Registration.Core/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Parts.Registration.Core
+{
+    public class Product : BaseEntity<Guid>
+    {
+        public Product(string name, string description, string partNumber, string createdBy)
+        {
+            this.Name = name.Trim();
+            this.Description = description.Trim();
+            this.PartNumber = partNumber.Trim();
+            this.Id = Guid.NewGuid();
+            this.CreationDate = DateTime.Now;
+            this.LastUpdate = DateTime.Now;
+            this.CreatedBy = createdBy.Trim();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PartNumber { get; set; }
+    }
+}
